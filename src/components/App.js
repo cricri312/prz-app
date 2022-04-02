ae8dc9bd88bc32fa4a6415b61b33790071cfbb59
cricri312 <t1m4c1n0@gmail.com>
2022-04-02 17:41:13 +0200

    Separate directory for files

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import '../css/App.css';
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      
+    }
+  }
+  render() {
+    return (
+      <div className="App">      
+      {/* <Navigation/>      
+      <Routes/> */}
+    </div>
+    );
+  }
+}
+
+export default App;
