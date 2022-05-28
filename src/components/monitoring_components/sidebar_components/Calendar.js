4b95f3a3adf88dba043a984d0e848c6f7c7833d4
cricri312 <t1m4c1n0@gmail.com>
2022-05-28 10:40:10 +0200

    Test Calendar

--- src/components/monitoring_components/sidebar_components/Calendar.js
+++ src/components/monitoring_components/sidebar_components/Calendar.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import DatePicker from 'react-mobile-datepicker';
-import {FcCalendar} from 'react-icons/fc'
+import { FcCalendar } from 'react-icons/fc'
 class Calendar extends Component {
 
 
   constructor(props) {
     super(props)
     this.state = {
-        time: new Date(),
-        is_open:false,
-        time_now: ""
+      time: new Date(),
+      is_open: false,
+      time_now: ""
     }
   }
 
@@ -21,12 +21,12 @@ class Calendar extends Component {
   render() {
     return (
       <div className='sidebar-calendar'>
-          <FcCalendar size={33} style={{cursor:"pointer"}} onClick={this.openDatePicker}/>
-          <DatePicker
-        //   value={this.state.time}
-        //   isOpen={this.state.is_open}
-        //   onSelect={this.handleSelect}
-        //   onCancel={this.handleCancel} 
+        <FcCalendar size={33} style={{ cursor: "pointer" }} onClick={this.openDatePicker} />
+        <DatePicker
+          value={this.state.time}
+          isOpen={this.state.is_open}
+          onSelect={this.handleSelect}
+          onCancel={this.handleCancel}
           theme="default"
           confirmText="Ok"
           cancelText="Cancel"
